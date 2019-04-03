138d104549128b89f75d4f50ef9fbdd06ee56ab9
xiaoweixiao <1679204951@qq.com>
2019-04-03 15:09:43 +0800

    divide candy

---
+++ 3_7_divide_candy.cc
@@ -0,0 +1,22 @@
+
+//3_7µÚÒ»Ìâ
+//#include<iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	int sub1, sub2, add1, add2;
+//	cin >> sub1 >> sub2 >> add1 >> add2;
+//	int A, B, C;
+//	B = (add1 - sub1) / 2;
+//	A = add1 - B;
+//	C = add2 - B;
+//	if (A != sub1 + B || C != B - sub2)
+//	{
+//		cout << "No" << endl;
+//		return 0;
+//	}
+//	cout << A << ' ' << B << ' ' << C << endl;
+//	system("pause");
+//	return 0;
+//}
